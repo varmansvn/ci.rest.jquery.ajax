a1e2c964219af9491741ede34489191b003f5063
varmansvn <varmansvn@gmail.com>
2012-03-28 01:08:23 +0700

    add ignore git file

--- application/logs/log-2012-03-27.php
+++ application/logs/log-2012-03-27.php
@@ -2010,3 +2010,48 @@ DEBUG - 2012-03-27 19:42:36 --> Session routines successfully run
 DEBUG - 2012-03-27 19:42:36 --> Controller Class Initialized
 DEBUG - 2012-03-27 19:42:36 --> Config file loaded: application/config/rest.php
 DEBUG - 2012-03-27 19:42:36 --> Helper loaded: inflector_helper
+DEBUG - 2012-03-27 20:00:37 --> Config Class Initialized
+DEBUG - 2012-03-27 20:00:37 --> Hooks Class Initialized
+DEBUG - 2012-03-27 20:00:37 --> Utf8 Class Initialized
+DEBUG - 2012-03-27 20:00:37 --> UTF-8 Support Enabled
+DEBUG - 2012-03-27 20:00:37 --> URI Class Initialized
+DEBUG - 2012-03-27 20:00:37 --> Router Class Initialized
+DEBUG - 2012-03-27 20:00:37 --> No URI present. Default controller set.
+DEBUG - 2012-03-27 20:00:37 --> Output Class Initialized
+DEBUG - 2012-03-27 20:00:37 --> Security Class Initialized
+DEBUG - 2012-03-27 20:00:37 --> Input Class Initialized
+DEBUG - 2012-03-27 20:00:37 --> Global POST and COOKIE data sanitized
+DEBUG - 2012-03-27 20:00:37 --> Language Class Initialized
+DEBUG - 2012-03-27 20:00:37 --> Loader Class Initialized
+DEBUG - 2012-03-27 20:00:37 --> Helper loaded: url_helper
+DEBUG - 2012-03-27 20:00:37 --> Unit Testing Class Initialized
+DEBUG - 2012-03-27 20:00:37 --> Session Class Initialized
+DEBUG - 2012-03-27 20:00:37 --> Helper loaded: string_helper
+DEBUG - 2012-03-27 20:00:37 --> Session routines successfully run
+DEBUG - 2012-03-27 20:00:37 --> Controller Class Initialized
+DEBUG - 2012-03-27 20:00:37 --> File loaded: application/views/header.php
+DEBUG - 2012-03-27 20:00:37 --> File loaded: application/views/all_users.php
+DEBUG - 2012-03-27 20:00:37 --> File loaded: application/views/footer.php
+DEBUG - 2012-03-27 20:00:37 --> File loaded: application/views/layout.php
+DEBUG - 2012-03-27 20:00:37 --> Final output sent to browser
+DEBUG - 2012-03-27 20:00:37 --> Total execution time: 0.1604
+DEBUG - 2012-03-27 20:00:39 --> Config Class Initialized
+DEBUG - 2012-03-27 20:00:39 --> Hooks Class Initialized
+DEBUG - 2012-03-27 20:00:39 --> Utf8 Class Initialized
+DEBUG - 2012-03-27 20:00:39 --> UTF-8 Support Enabled
+DEBUG - 2012-03-27 20:00:39 --> URI Class Initialized
+DEBUG - 2012-03-27 20:00:39 --> Router Class Initialized
+DEBUG - 2012-03-27 20:00:39 --> Output Class Initialized
+DEBUG - 2012-03-27 20:00:39 --> Security Class Initialized
+DEBUG - 2012-03-27 20:00:39 --> Input Class Initialized
+DEBUG - 2012-03-27 20:00:39 --> Global POST and COOKIE data sanitized
+DEBUG - 2012-03-27 20:00:39 --> Language Class Initialized
+DEBUG - 2012-03-27 20:00:39 --> Loader Class Initialized
+DEBUG - 2012-03-27 20:00:39 --> Helper loaded: url_helper
+DEBUG - 2012-03-27 20:00:39 --> Unit Testing Class Initialized
+DEBUG - 2012-03-27 20:00:39 --> Session Class Initialized
+DEBUG - 2012-03-27 20:00:39 --> Helper loaded: string_helper
+DEBUG - 2012-03-27 20:00:39 --> Session routines successfully run
+DEBUG - 2012-03-27 20:00:39 --> Controller Class Initialized
+DEBUG - 2012-03-27 20:00:39 --> Config file loaded: application/config/rest.php
+DEBUG - 2012-03-27 20:00:39 --> Helper loaded: inflector_helper
