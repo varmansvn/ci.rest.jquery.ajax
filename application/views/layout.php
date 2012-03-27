ac811d56f3b7b44031b052cbb46dbd6356f9b474
varmansvn <varmansvn@gmail.com>
2012-03-28 00:41:50 +0700

    remove single slash from url in layout.php

--- application/views/layout.php
+++ application/views/layout.php
@@ -1,8 +1,8 @@
 <html>
     <head>
         <title> Welcome to the demo application</title>
-        <link rel="stylesheet" href="<?php echo base_url(); ?>/css/main.css"></link>
-        <script src="<?php echo base_url(); ?>/js/jquery-latest.min.js" type="text/javascript"></script>         
+        <link rel="stylesheet" href="<?php echo base_url(); ?>css/main.css"></link>
+        <script src="<?php echo base_url(); ?>js/jquery-latest.min.js" type="text/javascript"></script>         
     </head>
     <body>
         <div id="header"><?php $this->load->view('header'); ?></div>
